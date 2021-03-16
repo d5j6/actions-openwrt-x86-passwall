d59f0aefbdc9832b814a538750a153b5a2e7f3e7
Dre Johnson <dre@phreshr.com>
2021-03-17 02:22:03 +0800

    fix typo

--- diy.sh
+++ diy.sh
@@ -13,13 +13,13 @@ mkdir package/luci-app-openclash
 cd package/luci-app-openclash
 git init
 git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true 
-echo  " luci-app-openclash "  >> .git/info/sparse-checkout
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
 git pull origin master
 git branch --set-upstream-to=origin/master master
 
-# Compiler po2lmo (if po2lmo skippable) 
-the pushd luci-App-openclash /tools/po2lmo
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd luci-app-openclash/tools/po2lmo
 make && sudo make install
 popd
 
