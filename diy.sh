9765e09a066bb768487b9e57623785bce5c904e2
Dre Johnson <dre@phreshr.com>
2021-03-16 20:33:38 +0800

    added passwall and openclash

--- diy.sh
+++ diy.sh
@@ -4,3 +4,22 @@ cp -R ../default-settings package/default-settings
 
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+rm -rf package/openwrt-passwall/xray-core
+
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true 
+echo  " luci-app-openclash "  >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+
+# Compiler po2lmo (if po2lmo skippable) 
+the pushd luci-App-openclash /tools/po2lmo
+make && sudo make install
+popd
+
+cd ../..
+make package/luci-app-openclash/luci-app-openclash/compile V=99
