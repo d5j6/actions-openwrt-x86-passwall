19310f4de5ed63590200f69c8bd5fe7b00747717
Dre Johnson <dre@phreshr.com>
2021-03-30 09:13:56 +0800

    update and use upx patch

--- diy.sh
+++ diy.sh
@@ -2,10 +2,8 @@
 
 cp -R ../default-settings package/default-settings
 
-git clone -b master --depth 1 https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone --depth 1 https://github.com/yichya/luci-app-xray.git package/luci-app-xray
 
 rm -rf package/openwrt-passwall/xray-core
 
